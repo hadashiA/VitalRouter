b1318eb5d34793e64ae45f84098196d6547fcdfd
hadashiA <dev@hadashikick.jp>
2024-10-13 22:20:14 +0900

    Add fuzzy state matcher

--- src/VitalRouter.Unity/Assets/VitalRouter.Tests/MRubySharedStateTest.cs
+++ src/VitalRouter.Unity/Assets/VitalRouter.Tests/MRubySharedStateTest.cs
@@ -48,6 +48,30 @@ namespace VitalRouter.Tests
             Assert.That(sharedState.TryGet<string>("string_value", out _), Is.False);
             Assert.That(context.Evaluate<string>("state[:string_value]"), Is.Null);
         }
+
+        [Test]
+        public void FuzzyMatcher()
+        {
+            using var context = MRubyContext.Create(Router.Default, new DummyCommandPreset());
+            var sharedState = new MRubySharedState(context);
+
+            sharedState.Set("a", "bar");
+            sharedState.Set("b", 123);
+            sharedState.Set("c", 1234.0f);
+            sharedState.Set("d", true);
+            sharedState.Set("e", false);
+
+            Assert.That(context.Evaluate<bool>("state[:a].is?(:bar)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:a].is?('bar')"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:b].is?(123)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:c].is?(1234.0)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:d].is?(true)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:e].is?(false)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:e].is?(nil)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:e].is?(true)"), Is.False);
+            Assert.That(context.Evaluate<bool>("state[:f].is?(nil)"), Is.True);
+            Assert.That(context.Evaluate<bool>("state[:f].is?(false)"), Is.True);
+        }
     }
 }
 #endif
